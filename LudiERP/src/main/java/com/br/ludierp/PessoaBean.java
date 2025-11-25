6dfbbb637e7f00b18a6e6b5653e87dc94eef0a03
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-25 19:02:07 -0300

    Usando List util para listar nome digitados no input  com anotação Request

---
+++ LudiERP/src/main/java/com/br/ludierp/PessoaBean.java
@@ -0,0 +1,47 @@
+package com.br.ludierp;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@RequestScoped
+@ManagedBean(name="pessoaBean")
+public class PessoaBean implements Serializable {
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+ private String nome;
+ 
+ //Lista	
+ private List<String> nomes = new ArrayList<String>();
+ 
+ //Método 
+ public String addNome() {
+	 nomes.add(nome);
+	 return "";
+ }
+ 
+ 
+ //Getters e Setters
+ public String getNome() {
+	return nome;
+}
+ public void setNome(String nome) {
+	this.nome = nome;
+}
+ 
+ public List<String> getNomes() {
+	return nomes;
+}
+ 
+ public void setNomes(List<String> nomes) {
+	this.nomes = nomes;
+}
+ 
+}
