98a2534f3db105f8cdce906f9e7a74ae0de1c52e
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-11-26 14:47:06 -0300

    Navegando a pagina dinamicamente com condições e retorna para página navegada

--- LudiERP/src/main/java/com/br/ludierp/PessoaBean.java
+++ LudiERP/src/main/java/com/br/ludierp/PessoaBean.java
@@ -38,8 +38,9 @@ public class PessoaBean implements Serializable {
 	 //Mais de 3 nomes desabilitar botão de comando 
 	 if (nomes.size()> 3 ) {
 		 commandButton.setDisabled(true);
+		 return "login";
 	 }
-	 return"";
+	 return ""; //Retorno null ou vazio mesma página
  }
  
  
